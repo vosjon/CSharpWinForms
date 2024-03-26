3f37c4824670553b568eabed7cd7780cd057a718
Jonathan Vos <vosjonjoe@gmail.com>
2024-03-26 12:07:15 -0700

    Started first section of the course.

---
+++ ConsoleApp.Demos/ConsoleApp.OutputDemo/Program.cs
@@ -0,0 +1,7 @@
+﻿// Video 14: Producing Output to Users
+Console.WriteLine("Hello, World! My name is Jonathan Vos");
+
+Console.WriteLine("I am from Redondo Beach, CA!");
+
+Console.Write("Line 1");
+Console.Write("Line 2");
---
+++ ConsoleApp.Demos/ConsoleApp.VariablesDemo/Program.cs
@@ -0,0 +1,10 @@
+﻿// Video 16: Variables Part 1
+
+string name = "Jonathan Vos";
+
+Console.WriteLine("I am " + name + "."); // Concatenation
+Console.WriteLine($"They call me {name}."); // String interpolation
+Console.WriteLine("I was given the name {0}.", name); // Formatted string
+
+int age = 36;
+Console.WriteLine(age);
