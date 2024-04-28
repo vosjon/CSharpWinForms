1581a71dfd8bc588c2bfe58555adb90fbe0c7107
Jonathan Vos <vosjonjoe@gmail.com>
2024-04-28 15:16:16 -0700

    Started the conditional statements section.

---
+++ ConsoleApp.Demos/ConsoleApp.ConditionsAndDecisions/Program.cs
@@ -0,0 +1,15 @@
+﻿// Section 7 Conditional Statements
+
+// Prompt for input
+Console.Write("Please enter a student's grade: ");
+int grade = Convert.ToInt32(Console.ReadLine());
+
+// Decide to print pass or fail based on input
+if (grade > 50) 
+{
+    Console.WriteLine("Pass");
+}
+else
+{
+    Console.WriteLine("Fail");
+}
